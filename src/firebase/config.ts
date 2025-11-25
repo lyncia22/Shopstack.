593df1d6130e37ae7dad30b2e70260f82e2540ad
Lyncia Chiguri <chigurilyncia@gmail.com>
2025-11-25 12:13:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9734879779-b4a8a",
+  "appId": "1:68482785538:web:20a3a1d78209dbd839e21b",
+  "apiKey": "AIzaSyAaXn-nXdiMJhA8b-nSHmaw-oFbSJlTVA0",
+  "authDomain": "studio-9734879779-b4a8a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "68482785538"
+};
